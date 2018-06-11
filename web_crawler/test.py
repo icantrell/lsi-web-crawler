dd02ba53d43af5256b0eb2e753e8a1265ab2a210
icantrell <icantrell617@gmail.com>
2018-06-11 15:47:10 -0700

    added more test. And fixed bugs. More functions for storage.

--- web_crawler/test.py
+++ web_crawler/test.py
@@ -1,35 +1,98 @@
-import web_crawler
-
-def test_webpage():
-    page = Webpage('https://en.wikipedia.org/wiki/Association_rule_learning#Algorithms')
-    assert(page.scheme)
-    assert(page.netloc)
-
-    page.fill()
-
-    page.parse()
-
-    for p in page.get_link_pages():
-        assert(page.scheme)
-        assert(page.netloc)
-        p.fill()
-
-    assert(page.english)
+from web_crawler import *
+import time
+    
 
 def test_webpage_not_english():
-    page = Webpage('')
+    page = Webpage('http://primes.utm.edu/lists/small/10000.txt')
 
     page.fill()
+    page.parse()
 
     assert(not page.english)
+
+def test_Queue():
+    q = Queue()
+    assert(q.randpop() == None)
+    q.append(1)
+    assert(q.randpop() == 1)
+    q.randpop()
+    assert(q.randpop() == None)
+    q.append(1)
+    q.append(2)
+    assert(q.randpop() in [1,2])
 
 def test_RM():
     p = Queue()
     up = Queue()
 
-    rm = RequestManager()
+    rm = RequestManager(up,p, use_init = False)
+    try:
+        rm.start()
 
-    rm.start()
+        up.append(Webpage('https://en.wikipedia.org/wiki/Singular-value_decomposition'))
+        time.sleep(3)
+        page = p.randpop()
+        assert(page)
+        page.fill()
+        page.parse()
+        assert(page.text)
+        if(page.english):
+            assert(page.links)
+        assert(rm.isAlive())
+        if(page.links):
+            assert(page.english)
+        rm.stop()
+        time.sleep(2)
+        assert(not rm.isAlive())
+    finally:
+        rm.stop()
+def test_Storage():
+    po = Queue()
+    up = Queue()
 
-    rm.unprocessed.append(Webpage(''))
-    rm.processed
+    s = Storage(po,up,dbfile='sqlite:///test_webpages.db')
+    try:
+        s.start()
+        page = Webpage('https://en.wikipedia.org/wiki/Singular-value_decomposition')
+        page.fill()
+        up.append(page)
+        time.sleep(2)
+        assert(len(po))
+        i = 0
+        for p in po:
+            if p.scheme and p.netloc:
+                i += 1
+        assert(i)
+        assert(s.isAlive())
+        s.stop()
+        time.sleep(2)
+        assert(not s.isAlive())
+    finally:
+        s.stop()
+
+def test_stop():
+    rm = RequestManager(Queue(),Queue(),use_init = False)
+    s = Storage(Queue(),Queue())
+    try:
+        rm.start()
+        s.start()
+        assert(rm.isAlive())
+        assert(s.isAlive())
+        rm.stop()
+        s.stop()
+        time.sleep(3)
+        assert(not rm.isAlive())
+
+        assert(not s.isAlive())
+    finally:
+        s.stop()
+        rm.stop()
+def test_select_query():
+    try:
+        s = Storage(Queue(), Queue(),dbfile='sqlite:///test_webpages.db')
+        s.truncate_table()
+        r = s.read_pages()
+        assert(r)
+        print(r)
+    finally:
+        s.stop()
--- web_crawler/web_crawler.py
+++ web_crawler/web_crawler.py
@@ -5,15 +5,16 @@ from sqlalchemy.orm import Session
 import random
 import urllib.request
 import threading
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Float, Text, create_engine
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Float, Text, create_engine, text as sa_text
 from sqlalchemy.ext.declarative import declarative_base
 MAXPROCESSED = 100
 MAXUNPROCESSED = 100
 
 NUM_STOPWORDS_IS_ENGLISH = 6
+STOP_WORD_FILE = 'stop_words.txt'
 Base = declarative_base()
 
-'''
+
 class Word(Base):
     __tablename__ = 'word'
     id = Column(Integer, primary_key = True)
@@ -26,7 +27,9 @@ class WordInstance(Base):
     frequency = Column(Float)
     webpage_id = Column(Integer, ForeignKey('webpage.id'))
     word_id = Column(Integer, ForeignKey('word.id'))
-'''
+
+
+
 class Webpage(HTMLParser,Base):
     stop_words = set()
     
@@ -35,7 +38,7 @@ class Webpage(HTMLParser,Base):
     url = Column(String)
     domain = Column(String)
     text = Column(Text)
-    stop_words = set(open(stop_word_file,'r').read().split())
+    stop_words = set(open(STOP_WORD_FILE,'r').read().split())
     
     def __init__(self, url ):
         HTMLParser.__init__(self)        
@@ -46,7 +49,7 @@ class Webpage(HTMLParser,Base):
         uri = urlparse(url)
         self.scheme = uri.scheme
         self.netloc = uri.netloc
-        self.domain = '{uri.scheme}://{uri.netloc}'.format(uri)
+        self.domain = '{uri.scheme}://{uri.netloc}'.format(uri = uri)
         
     def is_english(self):
         i=0
@@ -57,15 +60,15 @@ class Webpage(HTMLParser,Base):
                      return True
         return False
     
-    def fill(self, timeout = 1):
-        self.text = urllib.request.urlopen(url,timeout)
+    def fill(self, timeout = 5):
+        self.text = str(urllib.request.urlopen(url = self.url,timeout = timeout).read())
         
     def parse(self):
         self.feed(self.text)
         self.english = self.is_english()
     
-    def get_link_pages():
-        return [map(Webpage,self.links)]
+    def get_link_pages(self):
+        return list(map(Webpage,self.links))
     
     def handle_starttag(self,tag,attrs):
         if tag == 'a':
@@ -73,7 +76,7 @@ class Webpage(HTMLParser,Base):
                 if attrib[0] == 'href':
                     link = urlparse(attrib[1])
                     if not (link.scheme and  link.netloc):
-                        link = '{uri.scheme}://{uri.netloc}'.format(uri =  self.scheme + '://' +  self.netloc + attrib[1])
+                        link = '{uri.scheme}://{uri.netloc}'.format(uri = urlparse(self.scheme + '://' +  self.netloc + attrib[1]))
                         self.links.append(link)
                 else:
                     self.links.append(attrib[1])
@@ -92,24 +95,28 @@ class Queue(list):
     
     def randpop(self):
         with self.lock:
-            return super(Queue, self).pop(random.randint(0,len(self)-1))
+            if not len(self):
+                return None
+            return super(Queue, self).pop(len(self)!=1 if random.randint(0,len(self)-1) else 0)
 
 class RequestManager(threading.Thread):
-    def __init__(self, unprocessed, processed, init_urls_file = 'init_urls.txt', lf='RequestManager.log', domain_once = False):
+    def __init__(self, unprocessed, processed, use_init = True, init_urls_file = 'init_urls.txt', lf='RequestManager.log', domain_once = False):
         super(RequestManager, self).__init__()
         self.logfile = open(lf,'a')
         self.errors = 0
-        self.unprocessed = unnprocessed
-        self.unprocessed+= map(Webpage,open(init_urls_file,'r').read().split())
+        self.unprocessed = unprocessed
+        if use_init:
+            self.unprocessed+= map(Webpage,open(init_urls_file,'r').read().split())
         self.processed = processed
         self.closed_domains = set()
         self.closed_pages = set()
         self.do = domain_once
+        self._stop_event = threading.Event()
 
     def run(self):
         i = 0
-        while(1):
-            if len(self.processed) <= MAXPROCESSED: 
+        while(not self._stop_event.is_set()):
+            if len(self.processed) <= MAXPROCESSED and self.unprocessed:  
                 page= self.unprocessed.randpop()
                 try: 
                     page.fill()
@@ -119,35 +126,43 @@ class RequestManager(threading.Thread):
                 
                 self.closed_domains.add(page.domain)
                 self.closed_pages.add(page.url)
-                print('crawled ' + page.url + ' page ' + str(i))
-                print('domain ' + page.domain)
-                i += 1
+                if page.text:
+                    print('Request Manager: crawled ' + page.url + ' page ' + str(i))
+                    print('Request Manager: domain ' + page.domain)
+                    i += 1
         self.close()
 
     def close(self):
         pass
 
+    def stop(self):
+        self._stop_event.set()
 
 
 class Storage(threading.Thread):
-    def __init__(self, processed, unprocessed, logfile='storage.log', non_english = False):
+    def __init__(self, processed, unprocessed, logfile='storage.log',dbfile = 'sqlite:///webpages.db', non_english = False):
         super(Storage, self).__init__()
         #\\C:\\Users\\Issiah\\Envs\\lsi_web_scraper\\lsi-web-crawler\\web_crawler\\webpages.db
-        engine = create_engine('sqlite:///webpages.db')
+        engine = create_engine(dbfile)
+        self.engine = engine
         Base.metadata.create_all(engine)
         self.session = Session()
         self.logfile = open(logfile, 'a')
         self.processed = processed
         self.unprocessed = unprocessed
+        self._stop_event = threading.Event()
+        self.seen = set()
 
     def run(self):
-        while(1):
-            if (len(self.processed) <= MAXUNPROCESSED) and len(self.processed):
-                page = self.processed.rand_pop()
+        while(not self._stop_event.is_set()):
+            if (len(self.processed) <= MAXUNPROCESSED) and self.unprocessed:
+                page = self.unprocessed.randpop()
                 page.parse()
 
                 for linked_page in page.get_link_pages():
-                    self.unprocessed.append(linked_page)            
+                    if linked_page not in self.seen:
+                        self.processed.append(linked_page)
+                        self.seen.add(linked_page)
 
                 if page.english or non_english:
                     try:
@@ -161,11 +176,22 @@ class Storage(threading.Thread):
 
     def close(self):
         pass
+    
+    def stop(self):
+        self._stop_event.set()
 
+    
+    def read_pages(self):
+        return self.session.query(Webpage).all()
+
+    def truncate_table(self):
+        self.engine.execute(sa_text('''TRUNCATE TABLE webpage''').execution_options(autocommit=True))
+'''
 r = RequestManager()
-s = Storage(r)
+s = Storage()
 r.start()
 s.start()
 print("Starting crawling.")
 r.join()
 s.join()
+'''
